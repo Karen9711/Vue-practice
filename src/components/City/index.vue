b1db77f9334dc134e584bdd7ceff15ad479301d3
Karen9711 <bearbear199711@qq.com>
2019-07-03 09:41:00 +0800

    little change on city component

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -6,7 +6,16 @@
           热门城市
         </div>
         <div class="content">
-          成都   成都  成都
+          <button type="button" name="button">成都</button>
+          <button type="button" name="button">北京</button>
+          <button type="button" name="button">上海</button>
+          <button type="button" name="button">成都</button>
+          <button type="button" name="button">成都</button>
+          <button type="button" name="button">成都</button>
+          <button type="button" name="button">成都</button>
+          <button type="button" name="button">成都</button>
+          <button type="button" name="button">成都</button>
+          <button type="button" name="button">成都</button>
         </div>
       </div>
       <div class="normalCity">
@@ -121,9 +130,32 @@ export default {
 {
   background-color: #eeeeee;
   line-height: 2em;
+  padding: 3px;
+  font-weight: 700;
 }
 .city-list li
 {
   padding: 3px;
+  font-size: 14px;
+  margin-bottom: 3px;
+  margin-left: 5px;
+}
+.hotCity .content button
+{
+  outline: none;
+  border:1px solid #eee;
+  border-radius: 8px;
+  padding: 8px;
+  font-style: 14px;
+  width: 4em;
+  margin: 5px 15px;
+  background-color: #fff;
+}
+.hotCity .content
+{
+  display: flex;
+  flex-wrap:wrap;
+  justify-content: flex-start;
+  align-items:flex-start;
 }
 </style>
