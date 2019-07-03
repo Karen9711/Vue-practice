dc463fbde524aebbaaaa5e8687104396fcbf0fbb
Karen9711 <bearbear199711@qq.com>
2019-07-03 09:22:53 +0800

    add components

---
+++ src/components/Header/index.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <div id="header">
+
+    <h1>
+      <Icon type="ios-moon-outline" />
+      <Icon type="ios-star-outline" />
+       {{title}}
+       <Icon type="ios-star-outline" />
+       <Icon type="ios-moon-outline" />
+     </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Header',
+  props:{
+    title:{
+      type:String,
+      default:'要你好看电影'
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+#header
+{
+  background-color: #222831;
+  line-height:4em;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+#header h1
+{
+  text-align: center;
+  color:#eeeeee;
+  font-size: 20px;
+}
+</style>
---
+++ src/components/tabBar/index.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+  <div id="tabBar">
+    <router-link to='/movie' class="tab" tag="a">
+      <Icon type="md-film" size="30"/>
+      <div class="">
+        电影
+      </div>
+    </router-link>
+    <router-link to="/cinema" class="tab" tag="a">
+      <Icon type="md-home" size="30"/>
+      <div class="">
+        影院
+      </div>
+    </router-link>
+    <router-link to='/mine' class="tab" tag="a">
+      <Icon type="md-person" size="30"/>
+      <div class="">
+        我的
+      </div>
+    </router-link>
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+#tabBar
+{
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height:5em;
+  background-color:#222831;
+  color:#eeeeee;
+}
+#tabBar .tab
+{
+  display: inline-block;
+  text-align: center;
+  width: 32%;
+  margin-top:0.5em;
+  color:#eeeeee;
+}
+#tabBar .tab.router-link-active
+{
+  color:#d65a31;
+}
+</style>
